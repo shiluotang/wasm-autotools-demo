8188727d31dde1a645241a526452ea0c31df6c0c
Quangang Sheng <shengquangang2015@163.com>
2026-03-02 12:03:07 +0800

    add library

--- autogen.sh
+++ autogen.sh
@@ -3,9 +3,21 @@
 declare readonly CURDIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd -P)
 declare readonly TARGET=$(emcc -v 2>&1 | grep "Target" | sed -e "s/Target: //g")
 
+function mksdir() {
+    while test $# -gt 0 ; do
+        if [[ ! -d ${1} ]]; then
+            mkdir ${1}
+        fi
+        if ! shift 1; then
+            break
+        fi
+    done
+}
+
 function main() {
     local bindir=bin-${TARGET}
     pushd ${CURDIR} >& /dev/null
+    mksdir build-aux m4
     if [[ ! -f ./configure ]]; then
         autoreconf -vfi .
     fi
@@ -17,11 +29,14 @@ function main() {
         local em_pkg_dir=$(dirname $(emconfigure pkg-config --path egl))
         local pkg_dir=$(dirname $(env -i pkg-config --path egl))
         export EM_PKG_CONFIG_PATH=${em_pkg_dir}:${pkg_dir}
-        emconfigure ../configure --host=${TARGET}
+        if [[ ! -d ${HOME}/opt ]]; then
+            mkdir ${HOME}/opt
+        fi
+        emconfigure ../configure --host=${TARGET} --prefix=${HOME}/opt
         popd >& /dev/null
     fi
     if [[ -f ${bindir}/Makefile ]]; then
-        make -C ${bindir} clean all check V=1
+        emmake make -C ${bindir} clean all check V=1
     fi
     popd >& /dev/null
 }
---
+++ include/foo.h
@@ -0,0 +1,6 @@
+#ifndef FOO_H_INCLUDED
+#define FOO_H_INCLUDED
+
+void foo();
+
+#endif // FOO_H_INCLUDED
